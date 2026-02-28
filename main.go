b5e5185079b4f40bffd30984ddeb2cec2d58edd8
JakubKyhos <kubakyhos4@gmail.com>
2026-02-28 12:06:43 +0100

    added support for whitelist commands

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/JakubKyhos/Blockit.git/TLDs"
 	"github.com/JakubKyhos/Blockit.git/internal/database"
+	"github.com/JakubKyhos/Blockit.git/whitelist"
 	"github.com/joho/godotenv"
 )
 
@@ -43,28 +44,119 @@ func main() {
 			fmt.Println("-------------------------")
 			fmt.Println("DB has been set up successfully")
 			fmt.Println("-------------------------")
+		case "add":
+			if len(words) != 3 {
+				fmt.Println("add needs three arguments: add whitelist/whitelisttemp domainName")
+				continue
+			}
+			switch words[1] {
+			case "whitelist":
+				err = whitelist.CreateWhitelist(dbQueries, words[2])
+				if err != nil {
+					fmt.Printf("failed to add %v to DB: %v", words[2], err)
+					continue
+				}
+				fmt.Println("-------------------------")
+				fmt.Printf("%v has been added to whitelist successfully", words[2])
+				fmt.Println("-------------------------")
+			case "whitelisttemp":
+				err = whitelist.CreateWhitelistTemp(dbQueries, words[2])
+				if err != nil {
+					fmt.Printf("failed to add %v to DB: %v", words[2], err)
+					continue
+				}
+				fmt.Println("-------------------------")
+				fmt.Printf("%v has been added temporarily to whitelist", words[2])
+				fmt.Println("-------------------------")
+			default:
+				fmt.Println("Unknown argument. Use whitelist or whitelisttemp.")
+			}
+		case "delete":
+			if len(words) != 3 {
+				fmt.Println("delete needs three arguments: delete whitelist domainName/temp")
+				continue
+			}
+			switch words[1] {
+			case "whitelist":
+				if words[2] == "temp" {
+					err = whitelist.DeleteWhitelistTempDom(dbQueries)
+					if err != nil {
+						fmt.Printf("failed to delete old temporary domains from whitelist: %v", err)
+						continue
+					}
+				} else {
+					err = whitelist.DeleteWhitelistDom(dbQueries, words[2])
+					if err != nil {
+						fmt.Printf("failed to delete domain %v from whitelist: %v", words[2], err)
+						continue
+					}
+				}
+			default:
+				fmt.Println("Unknown argument. Use whitelist.")
+			}
 		case "list":
-			dblist, err := TLDs.ListDomains(dbQueries)
-			if err != nil {
-				fmt.Printf("failed to list DB: %v\n", err)
+			if len(words) != 2 {
+				fmt.Println("list needs two arguments: list tld/whitelist")
 				continue
 			}
-			if len(dblist) == 0 {
-				fmt.Println("DB is empty use 'setup' to populate DB")
-				continue
+			switch words[1] {
+			case "whitelist":
+				dblist, err := whitelist.ListWhitelist(dbQueries)
+				if err != nil {
+					fmt.Printf("failed to list whitelist from DB: %v\n", err)
+					continue
+				}
+				if len(dblist) == 0 {
+					fmt.Println("whitelist is empty use 'add whitelist webpage' to populate whitelist")
+					continue
+				}
+				fmt.Printf("%v\n", dblist)
+				for _, okweb := range dblist {
+					fmt.Println("-------------------------")
+					whitelist.PrintWhitelist(okweb)
+					fmt.Println("-------------------------")
+				}
+			case "tld":
+				dblist, err := TLDs.ListDomains(dbQueries)
+				if err != nil {
+					fmt.Printf("failed to list tld's from DB: %v\n", err)
+					continue
+				}
+				if len(dblist) == 0 {
+					fmt.Println("tld's DB is empty, use 'setup' to populate it")
+					continue
+				}
+				fmt.Printf("%v\n", dblist)
+				for _, domain := range dblist {
+					fmt.Println("-------------------------")
+					TLDs.PrintDomain(domain)
+					fmt.Println("-------------------------")
+				}
+			default:
+				fmt.Println("Unknown argument. Use tld or whitelist.")
 			}
-			fmt.Printf("%v\n", dblist)
-			for _, domain := range dblist {
-				fmt.Println("-------------------------")
-				TLDs.PrintDomain(domain)
-				fmt.Println("-------------------------")
-			}
+
 		case "reset":
-			err = TLDs.ResetDomains(dbQueries)
-			if err != nil {
-				fmt.Printf("failed to reset DB: %v", err)
-				continue
+			if len(words) != 2 {
+				fmt.Println("reset requires two arguments: reset tld/whitelist")
 			}
+			switch words[1] {
+			case "tld":
+				err = TLDs.ResetDomains(dbQueries)
+				if err != nil {
+					fmt.Printf("failed to reset DB: %v", err)
+					continue
+				}
+			case "whitelist":
+				err = whitelist.ResetWhitelist(dbQueries)
+				if err != nil {
+					fmt.Printf("failed to reset DB: %v", err)
+					continue
+				}
+			default:
+				fmt.Println("Unknown argument. Use tld or whitelist.")
+			}
+
 		case "blockstate":
 			if len(words) != 3 {
 				fmt.Println("blockstate needs 3 arguments: blockstate true/false domainName/global")
